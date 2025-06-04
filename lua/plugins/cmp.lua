4587290d533fa8a9eaf8fe5e807a422de27f4942
kuro5726 <shu89602@icloud.com>
2025-06-05 00:42:46 +0900

    cmd

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,39 +1,78 @@
-return {
-  -- 補完エンジン nvim-cmp の設定
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter", -- 挿入モードに入ったときにプラグインをロード
-  dependencies = { -- nvim-cmp に必要な依存プラグイン
-    { "hrsh7th/cmp-buffer" }, -- 現在のバッファの内容を補完候補に含める
-    { "saadparwaiz1/cmp_luasnip" }, -- LuaSnip と nvim-cmp を統合
-    { "L3MON4D3/LuaSnip" }, -- スニペットエンジン LuaSnip
-    { "rafamadriz/friendly-snippets" }, -- 事前定義されたスニペットコレクション
-  },
-  config = function()
-    -- nvim-cmp の設定
-    local cmp = require("cmp") -- nvim-cmp のメインモジュールをロード
-    local luasnip = require("luasnip") -- LuaSnip のモジュールをロード
-    require("luasnip/loaders/from_vscode").lazy_load() -- VSCode スタイルのスニペットをロード
-
-    cmp.setup({
-      snippet = {
-        -- スニペット展開方法を定義
-        expand = function(args)
-          luasnip.lsp_expand(args.body) -- LuaSnip を使ってスニペットを展開
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4), -- 補完候補のドキュメントを上にスクロール
-        ["<C-f>"] = cmp.mapping.scroll_docs(4), -- 補完候補のドキュメントを下にスクロール
-        ["<C-Space>"] = cmp.mapping.complete(), -- 手動で補完候補を表示
-        ["<C-e>"] = cmp.mapping.abort(), -- 補完を中断して閉じる
-        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- 補完確定 (現在選択中の候補を使用)
-      }),
-      sources = cmp.config.sources({
-        { name = "luasnip", priority_weight = 20 }, -- LuaSnip を補完候補に含める
-      }, {
-        { name = "buffer" }, -- バッファの内容を補完候補に含める
-      }),
-    })
-  end,
+local M = {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-nvim-lua",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "saadparwaiz1/cmp_luasnip",
+        "L3MON4D3/LuaSnip",
+        "onsails/lspkind.nvim"
+    },
+    event = { "CmdlineEnter" },
 }
 
+M.config = function()
+    local cmp = require("cmp")
+    local lspkind = require("lspkind")
+    vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+    cmp.setup({
+        formatting = {
+            format = lspkind.cmp_format({
+                mode = "symbol",
+                maxwidth = 50,
+                ellipsis_char = "...",
+                before = function(entry, vim_item)
+                    return vim_item
+                end,
+            }),
+        },
+        snippet = {
+            expand = function(args)
+                require("luasnip").lsp_expand(args.body)
+            end,
+        },
+        window = {
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-Space>"] = cmp.mapping.complete(),
+            ["<C-e>"] = cmp.mapping.abort(),
+            ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+            { name = "nvim_lsp" },
+            { name = "nvim_lua" },
+            { name = "luasnip" }, -- For luasnip users.
+            -- { name = "orgmode" },
+        }, {
+            { name = "buffer" },
+            { name = "path" },
+        }),
+    })
+
+    cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+            { name = "buffer" },
+        },
+    })
+
+    cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline({
+            ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        }),
+        sources = cmp.config.sources({
+            { name = "path" },
+        }, {
+            { name = "cmdline" },
+        }),
+    })
+end
+
+return M
