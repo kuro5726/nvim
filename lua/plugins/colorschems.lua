1225b4fc1d18b3ff6f20cb20c61cc8d02ca0d095
kuro5726 <shu89602@icloud.com>
2025-06-05 08:20:17 +0900

    aa

--- lua/plugins/colorschems.lua
+++ lua/plugins/colorschems.lua
@@ -4,6 +4,17 @@ return {
   },
 
   {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    config = function()
+      require("dashboard").setup{
+        shortcut_type = "number",
+        shuffle_letter = true,
+      }
+    end,
+  },
+
+  {
     "nvim-lualine/lualine.nvim",
     opts = {},
   },
@@ -36,5 +47,19 @@ return {
         }
       })
     end
-  }
+  },
+
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+    opts = {},
+  },
 }
