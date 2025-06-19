af3708639675f6085e93a87028ac3340bcfe220f
Shuya Kuromatsu <shuya.kuromatsu.hj@furuno.co.jp>
2025-06-19 16:58:07 +0900

    modify tresitter setting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,19 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = "master",
-  lazy = false,
-  build = ":TSUpdate"
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    lazy = false,
+    build = ":TSUpdate",
+    dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    main = "nvim-treesitter.configs",
+    opts = {
+      highlight = { enable = true },
+    },
+  },
+  {
+    "nvim-treesitter-textobjects",
+    event = "CursorMoved",
+  },
 }
